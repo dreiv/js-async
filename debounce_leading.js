9cd08270c2ad636f4f6b5d9cd9d845a9deb103bd
dreiv <dreivoicu@gmail.com>
2018-08-15 22:35:19 +0300

    debounce and debounce leading

--- debounce_leading.js
+++ debounce_leading.js
@@ -1,14 +1,22 @@
-const debounce = (delay, fn) => {
+const leadingDebounce = (delay, fn) => {
 	let int
+	let storedArgs
 
-	return (...arg) => {
+	return (...args) => {
 		clearTimeout(int)
-		int = setTimeout(() => fn(...arg), delay)
+		if (!storedArgs) {
+			storedArgs = args
+		}
+
+		int = setTimeout(() => {
+			fn(...storedArgs)
+			storedArgs = undefined
+		}, delay)
 	}
 }
 
 const { log } = console
-const logDebounced = debounce(1000, log)
+const logDebounced = leadingDebounce(1000, log)
 
 setTimeout(() => logDebounced(1), 10)
 setTimeout(() => logDebounced(2), 20)
